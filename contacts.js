760b0fdae166fdb6fdbd2425832e812f08015a2b
root <root@picoctf>
2026-09-05 18:09:14 -0400

    phase 1

--- contacts.js
+++ contacts.js
@@ -1,7 +1,35 @@
 /**
- * getContactNames
-getOnlineContacts
-findContactById
-createContactSummaries
 setContactOnlineStatus
  */
+
+export function getContactNames(contacts) {
+    return contacts.map(({name}) => name);
+}
+
+export function getOnlineContacts(contacts) {
+    return contacts.filter(({online}) => online);
+}
+
+export function findContactById(contacts, id) {
+    const contact = contacts.find((c) => c.id === id);
+
+    if(contact) {
+        return contact;
+    }
+
+    return "Not Found";
+}
+
+export function createContactSummaries(contacts) {
+    return contacts.map(({name, online}) => {
+        // let since value will change depending on online
+        let online_str = "" ;
+        if(online) {
+            online_str = "online";
+        } else {
+            online_str = "offline";
+        }
+
+        return `${name} - ${online_str}`
+    });
+}
