ff207d13d9f8590263fbb55c2e0edb5cfc9a749b
root <root@picoctf>
2026-09-07 15:11:48 -0400

    phase 5

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 /**
- * Imports data and functions
-Implements runAction
 Defines handlers
 Calls the program’s functions and prints results
-
-
-[
-  "Alex — online — 1 unread",
-  "Jordan — offline — 1 unread",
-  "Sam — online — 0 unread",
-]
  */
+
 import { countUnreadMessages } from "./messages.js";
 
 function createInboxSummaries(contacts, messages) {
@@ -18,7 +10,13 @@ function createInboxSummaries(contacts, messages) {
 
         const status = online ? "online" : "offline";
         const unread_msgs = countUnreadMessages(messages, id);
-        
+
         return `${name} - ${status} - ${unread_msgs} unread`;
     });
+}
+
+function runAction(actionName, callback) {
+    console.log(`Running: ${actionName}`);
+    console.log(callback());
+    console.log(`Finished: ${actionName}`);
 }
