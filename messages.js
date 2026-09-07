f029be3387a6f5a2dd205dc1dc79be386d74877c
root <root@picoctf>
2026-09-07 14:32:12 -0400

    phase 2

--- messages.js
+++ messages.js
@@ -1,9 +1,21 @@
-/**
- * getMessagesForContact
-getUnreadMessages
-findFirstUnreadMessage
-formatMessages
+/*
 markMessageAsRead
 sendMessage
 countUnreadMessages
  */
+
+export function getMessagesForContact(messages, id) {
+    return messages.filter((msg) => msg.id === id);
+}
+
+export function getUnreadMessages(messages) {
+    return messages.filter(({read}) => !read);
+}
+
+export function findFirstUnreadMessage(messages) {
+    return messages.find(({read}) => !read);
+}
+
+export function formatMessages(messages) {
+    return messages.map(({sender, text}) => `${sender}: ${text}`);
+}
